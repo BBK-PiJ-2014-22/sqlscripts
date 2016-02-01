0afa37f1df4f713e5a3a878c60b102fa33cb8998
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2016-02-01 17:26:10 +0000

    machine integrity created

--- tutorial1/machine_integrity.sql
+++ tutorial1/machine_integrity.sql
@@ -1,1 +1,9 @@
-CREATE OR UPDATE TRIGGER 
+CREATE OR REPLACE TRIGGER machine_integrity
+BEFORE DELETE ON Machines
+FOR EACH ROW
+BEGIN
+    delete_oil_entry(:old.mchid);
+    delete_work_entry(:old.mchid);
+END;
+/
+    
